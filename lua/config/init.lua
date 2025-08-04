d77406eb03400b66346a17142acad978284f1a0f
Thomas Moen <thomas@moen.io>
2025-08-04 20:37:39 +0100

    Add colorsheme & opts

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,15 @@
 require("config.lazy")
 require("config.diagnostics")
 require("config.mappings")
+
+local o = vim.opt
+
+o.clipboard = "unnamedplus"
+o.cursorline = true
+o.ignorecase = true
+o.scrolloff = 10
+o.smartcase = true
+o.smartindent = true
+o.tabstop = 2
+o.updatetime = 250
+o.relativenumber = true
