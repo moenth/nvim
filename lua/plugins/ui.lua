41803c5c56a2066d9f792acfccfe62d7bffcfb80
Thomas Moen <thomas@moen.io>
2025-08-04 23:17:06 +0100

    Add ui

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,52 @@
+return {
+  { "nvim-lua/plenary.nvim", lazy = true },
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    opts = {},
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>n", "", desc = "Noice" },
+      { "<leader>nn", "<cmd>Noice all<cr>", desc = "Open Noice" },
+    },
+    opts = {
+      cmdline = {
+        view = "cmdline", -- classic cmdline at the botton
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      messages = {
+        view_search = false, -- do not show search count as virtualtext
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      vim.cmd("colorscheme vscode")
+    end,
+  },
+}
