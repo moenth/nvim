944a47e887d41d592b8ec89045af00516736a21c
Thomas Moen <thomas@moen.io>
2025-08-26 10:11:36 +0100

    Add textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -36,8 +36,7 @@ return {
   },
   dependencies = {
     "RRethy/nvim-treesitter-endwise", -- mainly for lua 'closing end' insertion
-    -- "mfussenegger/nvim-ts-hint-textobject",
-    -- "windwp/nvim-ts-autotag",
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   config = function(_, opts)
     ---@type TSConfig
@@ -52,6 +51,38 @@ return {
       },
       indent = { enable = true },
       autopairs = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
+          goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
+          goto_previous_start = { ["[m"] = "@function.outer", ["[["] = "@class.outer" },
+          goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" },
+        },
+        swap = {
+          enable = true,
+          swap_next = { ["<leader>>"] = "@parameter.inner" },
+          swap_previous = { ["<leader><"] = "@parameter.outer" },
+        },
+        lsp_interop = {
+          enable = true,
+          peek_definition_code = {
+            ["gD"] = "@function.outer",
+          },
+        },
+      },
     })
 
     -- require("nvim-ts-autotag").setup()
